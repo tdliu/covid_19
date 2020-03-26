6433e28de6c64589be39c323debd0c11e774c66f
tdliu <tliu.auckland@gmail.com>
2020-03-26 16:00:57 -0700

    Trying to fix issue with port

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
-mkdir -p ~/.streamlit
-echo “[server]
-headless = true
-port = $PORTenableCORS = false
-” > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
