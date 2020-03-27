8553a547f5dafbd24d7fe71d20208076756d2b2e
tdliu <tliu.auckland@gmail.com>
2020-03-27 12:19:38 -0700

    Changed underlying data to NY Times

--- ca_covid_19.py
+++ ca_covid_19.py
@@ -4,108 +4,125 @@ import datetime
 import altair as alt
 import pandas as pd
 
-# Get data
-response = requests.get('https://files.sfchronicle.com/project-feeds/covid19_us_cases_ca_by_county_.json')
-raw_data = response.json()
-clean_data = []
-for x in raw_data:
-    county = x['GEOGRAPHY']
-    category = x['CATEGORY']
-    for key in x.keys():
-        try:
-            date = datetime.datetime.strptime(key, "%m/%d/%y")
-            clean_data.append([county, category, date, int(x[key])])
-        except ValueError:
-            pass
-data_pd = pd.DataFrame(clean_data, columns=['geography', 'category', 'date', 'value'])
-counties = sorted(list(set(data_pd['geography'])))
-counties.remove('BAY AREA')
-counties.remove('CALIFORNIA')
+# Get data from NY Times
+
+# County data
+counties_data_url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+counties_df = pd.read_csv(counties_data_url)
+counties_df['date'] = pd.to_datetime(counties_df['date'])
+counties_df = counties_df[counties_df.county != 'Unknown']  # removing cases that have unknown counties
+counties = sorted(list(set(counties_df['county'])))
+states = sorted(list(set(counties_df['state'])))
+
+# State data
+states_data_url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
+states_df = pd.read_csv(states_data_url)
+states_df['date'] = pd.to_datetime(states_df['date'])
 
 # Headings
-st.header("Compare spread of Covid-19 among California counties")
-updated_at = raw_data[0]['c2p_pubdate']
-st.write("Data from SF Chronicle's Coronavirus Tracker. Updated at", updated_at, 'PST.')
-st.markdown(
-    "This is *not* meant to be a comprehensive dashboard. [SF Chronicle's Coronavirus Tracker] (https://projects.sfchronicle.com/2020/coronavirus-map/) already has great data for California. [NY Times] (https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html) and [John Hopkins] (https://coronavirus.jhu.edu/map.html) have comprehensive US and global visualisations.  \n\nI made this because I wanted an easy way to compare how coronavirus has spread in San Francisco, where I live, with other regions.")
+st.header("Compare spread of Covid-19 among US counties")
+st.write('Data as of', max(counties_df['date']).strftime("%B %d, %Y"))
+st.markdown("""
+    This is not meant to be a comprehensive dashboard. I made this because I wanted an easy way to compare
+    how coronavirus has spread at a more granular level. Thankfully, New York Times is publishing 
+    [county-level data] (https://github.com/nytimes/covid-19-data), which I use to build the visualisations below.
+    \n\nFor comprehensive and high-level visualisations, check out [NY Times] (https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html) 
+    and [John Hopkins] (https://coronavirus.jhu.edu/map.html).
+""")
 
 # Dataframe of counties with most cases
-display_df = data_pd[data_pd['geography'].isin(counties)]
+
+# Display data frame
 st.subheader("Counties with most cases")
-display_df = display_df.groupby(['geography', 'category'])['value'].sum().reset_index()
-display_df = display_df.pivot(index='geography', columns='category', values='value')
+st.markdown("Filter states by selecting below. Clear options to see all states.")
+options_states = st.multiselect("States", states, default=['California', 'New York', 'Washington'])
 
-# Get cases in last 3 days
-last_3_df = data_pd
-last_3_df = last_3_df[
-    (last_3_df['date'] > datetime.datetime.now() - datetime.timedelta(hours=72)) & (last_3_df['category'] == 'cases')]
-last_3_df = last_3_df.groupby(['geography', 'category'])['value'].sum().reset_index()
-last_3_df = last_3_df.pivot(index='geography', columns='category', values='value')
 
-# Merge dataframes
-display_df = display_df.merge(last_3_df, on='geography')
-# Compute % change in cases
-old_cases = display_df['cases_x'] - display_df['cases_y']
-display_df['perc_change'] = 100 * (display_df['cases_x'] - old_cases) / old_cases
-# Style dataframe
-display_df.columns = ['Cases', 'Deaths', 'Cases last 72h', '% change']
-display_df = display_df.sort_values(by='Cases', ascending=False)
-st.dataframe(display_df.style.highlight_max(axis=0), height=150)
+@st.cache
+def format_display_data(counties_df, states):
+    if len(options_states) == 0:
+        disp_df = counties_df.groupby(['county', 'state'])['cases'].sum().reset_index()
+    else:
+        disp_df = counties_df[counties_df['state'].isin(states)].groupby(['county', 'state'])[
+            'cases'].sum().reset_index()
+
+    disp_df_2 = \
+        counties_df[(counties_df['date'] > max(counties_df['date']) - datetime.timedelta(days=3))].groupby('county')[
+            'cases'].sum().reset_index()
+    disp_df = disp_df.merge(disp_df_2, on='county')
+    old_cases = disp_df['cases_x'] - disp_df['cases_y']
+    disp_df['perc_change'] = 100 * (disp_df['cases_x'] - old_cases) / old_cases
+    disp_df.columns = ['County', 'State', 'Cases', 'Cases last 72h', '% change']
+    disp_df = disp_df.sort_values(by='Cases', ascending=False)
+    return disp_df
+
+
+disp_df = format_display_data(counties_df, options_states)
+
+st.dataframe(disp_df.style.highlight_max(axis=0), height=150)
 
 # Widgets
-st.subheader("Compare counties")
-st.markdown("Add or remove counties below. There are additional options in the sidebar.")
-options_counties = st.multiselect("CA Counties (add or remove with selector below)", counties,
-                                  default=['San Francisco County', 'Los Angeles County'])
-geo_ms = st.sidebar.multiselect('Other regions:', ('CALIFORNIA', 'BAY AREA'), default='BAY AREA')
-options_counties.extend(geo_ms)
+st.subheader("Compare counties and states")
+st.markdown("Add or remove counties and states below. There are additional options in the sidebar.")
+options_counties = st.multiselect("Counties", counties,
+                                  default=['San Francisco', 'Los Angeles'])
+options_states = st.multiselect('States', states, default='California')
+
+# Side bar options
 start_date = st.sidebar.date_input('Start date', datetime.date(2020, 3, 1))
-category_radio = st.sidebar.radio("Category", ('Cases', 'Deaths'))
-if category_radio == 'Cases':
-    category = 'cases'
-else:
-    category = 'deaths'
+
+category = st.sidebar.radio("Category", ('Cases', 'Deaths'))
 
 
-# Create dataframe
-def format_plot_data(raw_df, geos, category='cases'):
-    df = raw_df[(raw_df['category'] == category) &
-                (raw_df['geography'].isin(geos)) &
-                (raw_df['date'] <= max(
-                    raw_df['date']))]  # Remove most recent date since some counties haven't updated yet
-    df['cum_value'] = df.sort_values(['geography', 'date'], ascending=True).groupby('geography')['value'].cumsum()
+@st.cache
+def format_plot_data(counties_df, counties, states_df, states):
+    df = counties_df[
+        (counties_df['county'].isin(counties))]  # Remove most recent date since some counties haven't updated yet
+    df.rename(columns={"county": "geo"}, inplace=True)
+    df.drop(columns=['state', 'fips'], inplace=True)
+    df2 = states_df[(states_df['state'].isin(states))]
+    df2.rename(columns={"state": "geo"}, inplace=True)
+    df2.drop(columns=['fips'], inplace=True)
+    df = df.append(df2, ignore_index=True)
+    df['cases_cum_value'] = df.sort_values(['geo', 'date'], ascending=True).groupby('geo')['cases'].cumsum()
+    df['deaths_cum_value'] = df.sort_values(['geo', 'date'], ascending=True).groupby('geo')['deaths'].cumsum()
     df = df[df['date'] > pd.to_datetime(start_date)]  # '2020-02-20']  # Remove early dates without much data
     return df
 
 
-filtered_df = format_plot_data(data_pd, options_counties, category)
-
-# Plot charts
-
-# plot new cases
-if category == 'cases':
+plot_df = format_plot_data(counties_df, options_counties, states_df, options_states)
+if category == 'Cases':
     st.subheader("New cases")
+    alt_lc = alt.Chart(plot_df).mark_line().encode(
+        x=alt.X('date', axis=alt.Axis(title='Date')),
+        y=alt.Y('cases', axis=alt.Axis(title='Count')),
+        color=alt.Color('geo', legend=alt.Legend(orient="top-left", fillColor='white'))
+    )
+    st.altair_chart(alt_lc, use_container_width=True)
+
+    st.subheader("Cumulative cases")
+    alt_lc = alt.Chart(plot_df).mark_line().encode(
+        x=alt.X('date', axis=alt.Axis(title='Date')),
+        y=alt.Y('cases_cum_value', axis=alt.Axis(title='Count')),
+        color=alt.Color('geo', legend=alt.Legend(orient="top-left", fillColor='white'))
+    )
+    st.altair_chart(alt_lc, use_container_width=True)
 else:
     st.subheader("New deaths")
-alt_lc = alt.Chart(filtered_df).mark_line().encode(
-    x=alt.X('date', axis=alt.Axis(title='Date')),
-    y=alt.Y('value', axis=alt.Axis(title='Count')),
-    color=alt.Color('geography', legend=alt.Legend(orient="top-left", fillColor='white'))
-)
-st.altair_chart(alt_lc, use_container_width=True)
+    alt_lc = alt.Chart(plot_df).mark_line().encode(
+        x=alt.X('date', axis=alt.Axis(title='Date')),
+        y=alt.Y('deaths', axis=alt.Axis(title='Count')),
+        color=alt.Color('county', legend=alt.Legend(orient="top-left", fillColor='white'))
+    )
+    st.altair_chart(alt_lc, use_container_width=True)
 
-# plot cumulative cases
-if category == 'cases':
-    st.subheader("Cumulative cases")
-else:
     st.subheader("Cumulative deaths")
-
-alt_lc = alt.Chart(filtered_df).mark_line().encode(
-    x=alt.X('date', axis=alt.Axis(title='Date')),
-    y=alt.Y('cum_value', axis=alt.Axis(title='Count')),
-    color=alt.Color('geography', legend=alt.Legend(orient="top-left", fillColor='white'))
-)
-st.altair_chart(alt_lc, use_container_width=True)
+    alt_lc = alt.Chart(plot_df).mark_line().encode(
+        x=alt.X('date', axis=alt.Axis(title='Date')),
+        y=alt.Y('deaths_cum_value', axis=alt.Axis(title='Count')),
+        color=alt.Color('county', legend=alt.Legend(orient="top-left", fillColor='white'))
+    )
+    st.altair_chart(alt_lc, use_container_width=True)
 
 st.write("----------")
 st.write("""
