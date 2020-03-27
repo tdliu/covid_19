9faded58a4c508fb25e8f8cfb0092d56b9265b6f
tdliu <tliu.auckland@gmail.com>
2020-03-27 13:00:34 -0700

    updated data source link

--- ca_covid_19.py
+++ ca_covid_19.py
@@ -126,5 +126,5 @@ else:
 
 st.write("----------")
 st.write("""
-    By [Tony Liu] (https://tonydl.com/) | source: [GitHub] (https://github.com/tdliu/covid_19) | data source [SF Chronicle] (https://projects.sfchronicle.com/2020/coronavirus-map/)
+    By [Tony Liu] (https://tonydl.com/) | source: [GitHub] (https://github.com/tdliu/covid_19) | data source: [NY Times] (https://github.com/nytimes/covid-19-data)
 """)
